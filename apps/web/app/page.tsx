42c89c05fcbee28488b97276d84bb74d6faff23e
Vaishnav verma <vaishnavxwork@gmail.com>
2026-02-22 14:19:47 +0530

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,7 +6,9 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
+      name:
       <div> {users?.username} </div>
+      password:
       <div> {users?.password} </div>
     </div>
   )
